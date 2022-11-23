2a34234620b9aef685ff1be9dde04d89d65f3ddb
Battulga-remzx <bayarbaatarbattulga5959@gmail.com>
2022-11-23 16:32:57 +0800

    first push

--- index.js
+++
@@ -1,7 +0,0 @@
-let a = document.querySelector('[data-start]');
-let res = document.getElementById("hha")
-a.forEach(button => {
-    button.addEventListener('click', () => {
-        res.innerText('clicked')
-    })
-})
